13a9a36f2ae457414d8c08229ec66ca1a598d769
Kiefer Hagin <kieferhagib@gmail.com>
2019-11-11 22:09:08 -0800

    timer test

--- emulator.py
+++ emulator.py
@@ -1,5 +1,8 @@
+import time
+
 from gameboy.gameboy import GameBoy
 from gameboy.rom import ROM
+from gameboy.timer_registers import TimerRegisters
 
 
 def main():
@@ -11,10 +14,28 @@ def main():
     game_boy = GameBoy()
     game_boy.load_rom(rom=test_rom)
 
+    # timer_test()
+
 
 def load_test_rom():
     with open("test_roms/instr_timing.gb", "rb") as binary_file:
         return ROM(bytearray(binary_file.read()))
+
+
+def timer_test():
+    print("Running timer test...")
+
+    def fake_set_interrupt():
+        print("Interrupt set!")
+
+    timer_registers = TimerRegisters(fake_set_interrupt)
+    timer_registers._data[3] = 0b00000110  # Enabled, 64
+    timer_registers._data[2] = 100  # Modulo
+
+    while True:
+        timer_registers.tick()
+        print(', '.join('{:02x}'.format(x) for x in timer_registers._data))
+        time.sleep(0.001)
 
 
 if __name__ == "__main__":
