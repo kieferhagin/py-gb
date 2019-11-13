e1699d799c27bae69f0af1ae7ed214eabfd4b963
Kiefer Hagin <kieferhagib@gmail.com>
2019-11-12 18:53:35 -0800

    finish interrupt flag register

--- gameboy/interrupt_flag_register.py
+++ gameboy/interrupt_flag_register.py
@@ -2,10 +2,51 @@ from gameboy.memory_region import MemoryRegion
 
 
 class InterruptFlagRegister(MemoryRegion):
+    INTERRUPT_VBLANK = 0x01
+    INTERRUPT_LCDC = 0x02
     INTERRUPT_TIMA = 0x04
+    INTERRUPT_SERIAL = 0x08
+    INTERRUPT_JOYPAD = 0x10
+
+    INTERRUPT_MASK = 0x1F
 
     def __init__(self):
         super().__init__(bytearray(1), 0xFF0F)
+        self._data[0] = 0xE0
+
+    def set_vblank_interrupt(self):
+        self._data[0] = self._data[0] | self.INTERRUPT_VBLANK
+
+    def set_lcdc_interrupt(self):
+        self._data[0] = self._data[0] | self.INTERRUPT_LCDC
 
     def set_tima_interrupt(self):
         self._data[0] = self._data[0] | self.INTERRUPT_TIMA
+
+    def set_serial_interrupt(self):
+        self._data[0] = self._data[0] | self.INTERRUPT_SERIAL
+
+    def set_joypad_interrupt(self):
+        self._data[0] = self._data[0] | self.INTERRUPT_JOYPAD
+
+    def clear_vblank_interrupt(self):
+        self._data[0] = self._data[0] & ~self.INTERRUPT_VBLANK
+
+    def clear_lcdc_interrupt(self):
+        self._data[0] = self._data[0] & ~self.INTERRUPT_LCDC
+
+    def clear_tima_interrupt(self):
+        self._data[0] = self._data[0] & ~self.INTERRUPT_TIMA
+
+    def clear_serial_interrupt(self):
+        self._data[0] = self._data[0] & ~self.INTERRUPT_SERIAL
+
+    def clear_joypad_interrupt(self):
+        self._data[0] = self._data[0] & ~self.INTERRUPT_JOYPAD
+
+    def write_byte(self, address: int, value: int):
+        # Top 4 bits of interrupt flags always read "1"s
+        return super().write_byte(address, value | 0xE0)
+
+    def get_interrupt_bits(self):
+        return self.read_byte(0xFF0F) & self.INTERRUPT_MASK
--- gameboy/interrupt_flag_register_test.py
+++ gameboy/interrupt_flag_register_test.py
@@ -10,8 +10,81 @@ def interrupt_flag_register_fixture():
 
 def test_interrupt_flag_register_init(interrupt_flag_register_fixture):
     assert len(interrupt_flag_register_fixture._data) == 1
+    assert interrupt_flag_register_fixture._data[0] == 0xE0
+    assert interrupt_flag_register_fixture._base_address == 0xFF0F
+
+
+def test_interrupt_flag_register_set_vblank(interrupt_flag_register_fixture):
+    interrupt_flag_register_fixture.set_vblank_interrupt()
+    assert interrupt_flag_register_fixture.read_byte(0xFF0F) == 0xE1
+    
+    
+def test_interrupt_flag_register_set_lcdc(interrupt_flag_register_fixture):
+    interrupt_flag_register_fixture.set_lcdc_interrupt()
+    assert interrupt_flag_register_fixture.read_byte(0xFF0F) == 0xE2
 
 
 def test_interrupt_flag_register_set_tima(interrupt_flag_register_fixture):
     interrupt_flag_register_fixture.set_tima_interrupt()
-    assert interrupt_flag_register_fixture.read_byte(0xFF0F) == 0x04
+    assert interrupt_flag_register_fixture.read_byte(0xFF0F) == 0xE4
+
+
+def test_interrupt_flag_register_set_serial(interrupt_flag_register_fixture):
+    interrupt_flag_register_fixture.set_serial_interrupt()
+    assert interrupt_flag_register_fixture.read_byte(0xFF0F) == 0xE8
+
+
+def test_interrupt_flag_register_set_joypad(interrupt_flag_register_fixture):
+    interrupt_flag_register_fixture.set_joypad_interrupt()
+    assert interrupt_flag_register_fixture.read_byte(0xFF0F) == 0xF0
+
+
+def test_clear_interrupt_flag_register_vblank(interrupt_flag_register_fixture):
+    interrupt_flag_register_fixture.set_vblank_interrupt()
+    interrupt_flag_register_fixture.clear_vblank_interrupt()
+
+    assert not interrupt_flag_register_fixture.read_byte(0xFF0F) & 0x01
+
+
+def test_clear_interrupt_flag_register_lcdc(interrupt_flag_register_fixture):
+    interrupt_flag_register_fixture.set_lcdc_interrupt()
+    interrupt_flag_register_fixture.clear_lcdc_interrupt()
+
+    assert not interrupt_flag_register_fixture.read_byte(0xFF0F) & 0x02
+    
+    
+def test_clear_interrupt_flag_register_tima(interrupt_flag_register_fixture):
+    interrupt_flag_register_fixture.set_tima_interrupt()
+    interrupt_flag_register_fixture.clear_tima_interrupt()
+
+    assert not interrupt_flag_register_fixture.read_byte(0xFF0F) & 0x04
+    
+    
+def test_clear_interrupt_flag_register_serial(interrupt_flag_register_fixture):
+    interrupt_flag_register_fixture.set_serial_interrupt()
+    interrupt_flag_register_fixture.clear_serial_interrupt()
+
+    assert not interrupt_flag_register_fixture.read_byte(0xFF0F) & 0x08
+    
+    
+def test_clear_interrupt_flag_register_joypad(interrupt_flag_register_fixture):
+    interrupt_flag_register_fixture.set_joypad_interrupt()
+    interrupt_flag_register_fixture.clear_joypad_interrupt()
+
+    assert not interrupt_flag_register_fixture.read_byte(0xFF0F) & 0x10
+
+
+def test_interrupt_register_write_byte_maintains_top_4_bit_1s(interrupt_flag_register_fixture):
+    interrupt_flag_register_fixture.write_byte(0xFF0F, 0x10)
+
+    assert interrupt_flag_register_fixture._data[0] == 0xF0
+
+
+def test_interrupt_register_get_interrupt(interrupt_flag_register_fixture):
+    interrupt_flag_register_fixture.set_vblank_interrupt()
+
+    assert interrupt_flag_register_fixture.get_interrupt_bits() == 0x01
+
+    interrupt_flag_register_fixture.set_lcdc_interrupt()
+
+    assert interrupt_flag_register_fixture.get_interrupt_bits() == 0x03
